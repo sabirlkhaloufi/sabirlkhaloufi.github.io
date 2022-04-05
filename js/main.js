7af9635da1711c3a3e2958ca2768e98419478078
sabir lkhaloufi <83829858+sabirlkhaloufi@users.noreply.github.com>
2022-04-05 09:31:38 +0000

    add partie services

--- js/main.js
+++ js/main.js
@@ -9,16 +9,8 @@ let typeWrite = setInterval(function(){
     }
 },100 )
 
-// let typeWrite = setInterval(function(){
-//     pWrite.innerHTML+=write[write.len];
-//     i= i+1;
-//     if(i>write.length - 1){
-//         clearInterval(typeWrite);
-//     }
-// },100 )
+/////////////////show navbar
 
-
-// show navbar
 let menuToggle = document.querySelector('.menutoggle');
 menuToggle.addEventListener("click",function(){
     document.querySelector('.t-line').classList.toggle('t-line-r');
@@ -26,3 +18,36 @@ menuToggle.addEventListener("click",function(){
     document.querySelector('.m-line').classList.toggle('cach-m-line');
     document.querySelector('.navbar-mobile').classList.toggle('show-navbar-mobile');
 })
+
+/////////////////up to top
+
+
+const up = document.querySelector(".up-top");
+
+window.addEventListener("scroll",()=>{
+    if(window.scrollY >200){
+        up.classList.add("show-up-top");
+        document.querySelector(".bg-nav").classList.add("show-bg-nav");
+    }
+    else{
+        up.classList.remove("show-up-top"); 
+        document.querySelector(".bg-nav").classList.remove("show-bg-nav");
+    }
+})
+
+up.addEventListener("click",()=>{
+    document.documentElement.scrollTop=0;
+})
+
+/////////////////////hover pour des carde services
+const cardServices = document.querySelectorAll(".card");
+
+cardServices.forEach(card => {
+    card.addEventListener("mouseover",()=>{
+        card.children[0].style.color = "white";
+    })
+    card.addEventListener("mouseout",()=>{
+        card.children[0].style.color = "crimson";
+    })
+
+});
